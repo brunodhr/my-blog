451ba645aff5c9d3f0e2a28262b86a0f4ef40997
bruno <brunofiliperamos@hotmail.com>
2019-09-25 12:01:57 -0300

    chore: add utils and templates to gatsby alias

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,8 +5,10 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        components: path.resolve(__dirname, "src/components"),
-        pages: path.resolve(__dirname, "src/pages"),
+        components: path.resolve(__dirname, 'src/components'),
+        pages: path.resolve(__dirname, 'src/pages'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        templates: path.resolve(__dirname, 'src/templates'),
       },
     },
   })
