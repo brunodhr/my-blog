9708dd7f900ecae2d7b8eaf61d0e4288fc6eec8f
bruno <brunofiliperamos@hotmail.com>
2019-09-26 14:48:43 -0300

    chore: config google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-148797965-1`,
       },
     },
     {
