5740476f06384347400e8fefd428887ae907c55e
bruno <brunofiliperamos@hotmail.com>
2019-09-26 15:19:05 -0300

    refac(Post): reset style

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import GlobalStyle from '../styles'
 
 import Bio from 'components/Bio'
@@ -33,32 +33,22 @@ class BlogPostTemplate extends React.Component {
             </footer>
           </article>
 
-          <nav>
-            <ul
-              style={{
-                display: `flex`,
-                flexWrap: `wrap`,
-                justifyContent: `space-between`,
-                listStyle: `none`,
-                padding: 0,
-              }}
-            >
-              <li>
-                {previous && (
-                  <Link to={previous.fields.slug} rel='prev'>
-                    ← {previous.frontmatter.title}
-                  </Link>
-                )}
-              </li>
-              <li>
-                {next && (
-                  <Link to={next.fields.slug} rel='next'>
-                    {next.frontmatter.title} →
-                  </Link>
-                )}
-              </li>
-            </ul>
-          </nav>
+          <S.Nav>
+            <S.Arrows>
+              {previous && (
+                <S.StyledLink to={previous.fields.slug} rel='prev'>
+                  ← {previous.frontmatter.title}
+                </S.StyledLink>
+              )}
+            </S.Arrows>
+            <S.Arrows>
+              {next && (
+                <S.StyledLink to={next.fields.slug} rel='next'>
+                  {next.frontmatter.title} →
+                </S.StyledLink>
+              )}
+            </S.Arrows>
+          </S.Nav>
         </Layout>
       </>
     )
--- src/templates/styles.js
+++ src/templates/styles.js
@@ -1,3 +1,15 @@
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
 export const Container = styled.div``
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const Arrows = styled.div``
+
+export const StyledLink = styled(Link)`
+  color: rgb(97, 219, 251);
+`
