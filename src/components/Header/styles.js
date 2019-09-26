d96f99119a348ff5af48e5f2a756f0a633a83270
bruno <brunofiliperamos@hotmail.com>
2019-09-26 08:11:45 -0300

    feat(Header): initial component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,46 @@
+import styled, { keyframes } from 'styled-components'
+
+const rotate = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+`
+
+export const Menu = styled.header`
+  background-color: #505050;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20%;
+  @media (min-width: 320px) and (max-width: 425px) {
+    flex-direction: column;
+    max-height: 80px;
+    overflow: hidden;
+  }
+`
+
+export const Logo = styled.svg`
+  padding: 10px;
+  width: 80px;
+  height: 80px;
+  transition: 0.3s;
+  opacity: 0.5;
+  &:hover {
+    opacity: 1;
+  }
+`
+
+export const Github = styled(Logo)`
+  animation: ${rotate} infinite 20s linear;
+  fill: rgb(97, 219, 251);
+`
+
+export const Gatsby = styled(Logo)`
+  width: 150px;
+  fill: #639;
+`
+
+export const Link = styled.a``
