ca4f505aac667f48eaa1f468a36c2dc6b61d63c5
bruno <brunofiliperamos@hotmail.com>
2019-09-26 08:13:01 -0300

    refac(Layout): componentized Header and Footer and style

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,3 +1,22 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Wrapper = styled.div`
+  height: 100vh;
+  background-color: grey;
+`
+// background-image: linear-gradient(to bottom right, #505050, #81c2de);
+
+export const Body = styled.div`
+  padding: 20px 20%;
+  height: calc(100vh - 133px);
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: #505050;
+  }
+  @media (min-width: 320px) and (max-width: 425px) {
+    height: calc(100vh - 130px);
+  }
+`
