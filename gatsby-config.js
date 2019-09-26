9ce1b57be818992a3f2e4bad050c55a426a64b62
bruno <brunofiliperamos@hotmail.com>
2019-09-26 10:39:52 -0300

    chore: rm typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,11 +70,5 @@ module.exports = {
         icon: `content/assets/gatsby-icon.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
   ],
 }
