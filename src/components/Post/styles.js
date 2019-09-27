af236d2f614d6283dedbb74fcc5996ac70c8dad6
bruno <brunofiliperamos@hotmail.com>
2019-09-27 17:01:38 -0300

    fix(Post): responsive mobile/tablet layout

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -3,11 +3,8 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   padding: 10px;
   width: 50%;
-  @media (min-width: 320px) and (max-width: 425px) {
-    width: 50%;
-  }
-  @media (min-width: 426px) and (max-width: 1023px) {
-    width: 33%;
+  @media (min-width: 320px) and (max-width: 1023px) {
+    width: 100%;
   }
 `
 
