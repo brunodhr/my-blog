3de705f3c60745c589122a224a061e3fcef2f4b9
bruno <brunofiliperamos@hotmail.com>
2019-09-30 10:41:27 -0300

    chore(dep): add transition link plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-transition-link`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-offline`,
