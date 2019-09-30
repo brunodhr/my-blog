ba2fd3a68ca7cb638a8712435bacc19ea3abb2b9
bruno <brunofiliperamos@hotmail.com>
2019-09-30 12:52:30 -0300

    feat(Post): styled new component Link

--- src/templates/styles.js
+++ src/templates/styles.js
@@ -1,15 +1,22 @@
 import styled from 'styled-components'
-import { Link } from 'gatsby'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
 
 export const Container = styled.div``
 
 export const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
+  margin: 20px 0;
 `
 
 export const Arrows = styled.div``
 
-export const StyledLink = styled(Link)`
+export const Link = styled(AniLink)`
   color: rgb(97, 219, 251);
+  font-weight: 900;
+  opacity: 0.5;
+  transition: 0.2s;
+  &:hover {
+    opacity: 1;
+  }
 `
